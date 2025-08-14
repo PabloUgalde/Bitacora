7a85227ce632fc9572a5c7d86b2bf6fe90921f36
PabloUgalde <66987917+PabloUgalde@users.noreply.github.com>
2025-08-14 11:39:39 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flight-log-cache-v1';
+const CACHE_NAME = 'flight-log-cache-v1.0';
 // Lista de archivos que componen el "cascarón" de la aplicación.
 const APP_SHELL_FILES = [
     './index.html',
